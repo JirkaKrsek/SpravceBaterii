eb835cc050259e8f6abec4828cedac230bf41896
JiriKrsek <krsek.jirka@gmail.com>
2025-06-04 12:32:06 +0200

    Založení AppDbContext

---
+++ SpravceBaterii/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpravceBaterii.Data
+{
+    /// <summary>
+    /// Třída pro správu databáze pomocí závislosti Microsoft.EntityFrameworkCore
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        /// <summary>
+        /// Konstruktor s konfigurací připojení
+        /// </summary>
+        /// <param name="options"></param>
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
