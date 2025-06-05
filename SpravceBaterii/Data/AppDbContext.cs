2ea75ad465249d6c193e554b23d7c95980655b61
JiriKrsek <krsek.jirka@gmail.com>
2025-06-05 21:09:33 +0200

    Přidání entitových modelů do AppDbContext

--- SpravceBaterii/Data/AppDbContext.cs
+++ SpravceBaterii/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SpravceBaterii.Data.Models;
 
 namespace SpravceBaterii.Data
 {
@@ -14,5 +15,15 @@ namespace SpravceBaterii.Data
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Battery> Batteries { get; set; }
+        public DbSet<BatteryType> BatteryTypes { get; set; }
+        public DbSet<ChemicalComposition> ChemicalCompositions { get; set; }
+        public DbSet<DisposableBattery> DisposableBatteries { get; set; }
+        public DbSet<RechargeableBattery> RechargeableBatteries { get; set; }
+        public DbSet<ChargingHistory> ChargingHistories { get; set; }
     }
 }
