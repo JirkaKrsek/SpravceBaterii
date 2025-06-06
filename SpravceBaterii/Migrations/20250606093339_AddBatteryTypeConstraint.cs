b2b6ed60853a6ea289b05ac0834cd56238b5d2bd
JiriKrsek <krsek.jirka@gmail.com>
2025-06-06 13:50:38 +0200

    Vytvoření migrace - přidání CHECK CONSTRAINT pro typ baterie (jednorázová/nabíjecí)

---
+++ SpravceBaterii/Migrations/20250606093339_AddBatteryTypeConstraint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpravceBaterii.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBatteryTypeConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Batteries ADD CONSTRAINT Check_Battery_OnlyOneType " +
+            "CHECK ((IsRechargeable = 0 AND DisposableBatteryId IS NOT NULL AND RechargeableBatteryId IS NULL) OR " +
+            "(IsRechargeable = 1 AND DisposableBatteryId IS NULL AND RechargeableBatteryId IS NOT NULL))");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Batteries DROP CONSTRAINT Check_Battery_OnlyOneType");
+        }
+    }
+}
