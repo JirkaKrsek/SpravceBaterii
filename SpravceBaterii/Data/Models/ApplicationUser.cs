368a8fb7609fe98262eb96ea1b99be83f651aa83
JiriKrsek <krsek.jirka@gmail.com>
2025-06-08 14:23:22 +0200

    Vytvoření entitní třídy uživatele využívající závislost Microsoft.Identity

---
+++ SpravceBaterii/Data/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SpravceBaterii.Data.Models
+{
+    /// <summary>
+    /// Modelová třída dědící z IdentityUser ze závislosti Microsoft.Identity
+    /// Přidání dalších potřebných atributů
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastLogin { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Location>? Locations { get; set; }
+    }
+}
