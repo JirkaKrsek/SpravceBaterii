c3d0970ffa0b183ede2ed7dae869bc8e08066e38
JiriKrsek <krsek.jirka@gmail.com>
2025-09-08 08:46:18 +0200

    Vytvoření služby BatteryService - načtení baterií uživatele z databáze

---
+++ SpravceBaterii/Services/BatteryService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpravceBaterii.Data;
+using SpravceBaterii.Data.Models;
+
+namespace SpravceBaterii.Services
+{
+    public class BatteryService
+    {
+        private ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="applicationDbContext">ApplicationDbContext</param>
+        public BatteryService(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// Načtení baterií pro daného uživatele podle jeho ID
+        /// </summary>
+        /// <param name="userId">ID uživatele</param>
+        /// <returns>List baterií</returns>
+        public async Task<List<Battery>> GetUserBatteries(string userId)
+        {
+            return await ApplicationDbContext.Batteries.Where(b => b.UserId == userId).ToListAsync();
+        }
+    }
+}
