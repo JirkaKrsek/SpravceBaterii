3a8d86099bd067baad95340e4a9e6fc3fff7c317
JiriKrsek <krsek.jirka@gmail.com>
2025-09-09 12:33:36 +0200

    Vytvoření služby BatteryTypeService - načtení typů baterií

---
+++ SpravceBaterii/Services/BatteryTypeService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpravceBaterii.Data;
+using SpravceBaterii.Data.Models;
+
+namespace SpravceBaterii.Services
+{
+    public class BatteryTypeService
+    {
+        private ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="applicationDbContext">ApplicationDbContext</param>
+        public BatteryTypeService(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// Načtení všech typů baterií
+        /// </summary>
+        /// <returns>List typů baterií</returns>
+        public async Task<List<BatteryType>> GetBatteryTypes()
+        {
+            return await ApplicationDbContext.BatteryTypes.ToListAsync();
+        }
+    }
+}
