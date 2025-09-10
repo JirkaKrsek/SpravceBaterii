def0347ded2a3162e4e44065df960dd1d12a5f9e
JiriKrsek <krsek.jirka@gmail.com>
2025-09-10 17:20:51 +0200

    Vytvoření služby ApplicationUserService - získání ID přihlášeného uživatele

---
+++ SpravceBaterii/Services/ApplicationUserService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace SpravceBaterii.Services
+{
+    public class ApplicationUserService
+    {
+        private readonly AuthenticationStateProvider AuthenticationStateProvider;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="authenticationStateProvider">AuthenticationStateProvider</param>
+        public ApplicationUserService(AuthenticationStateProvider authenticationStateProvider)
+        {
+            AuthenticationStateProvider = authenticationStateProvider;
+        }
+
+        /// <summary>
+        /// Získání ID přihlášeného uživatele
+        /// </summary>
+        /// <returns>string? ID</returns>
+        public async Task<string?> GetUserIdAsync()
+        {
+            AuthenticationState state = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            return state.User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
