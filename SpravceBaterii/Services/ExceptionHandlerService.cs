0886cee5ef7695bd47e8be0a8a762e3bf6fccd4b
JiriKrsek <krsek.jirka@gmail.com>
2025-09-11 13:58:58 +0200

    Vytvoření ExceptionHandlerService pro centrální zpracování výjimek

---
+++ SpravceBaterii/Services/ExceptionHandlerService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpravceBaterii.Services
+{
+    public class ExceptionHandlerService
+    {
+        private readonly NavigationManager Navigation;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="navigation">NavigationManager</param>
+        public ExceptionHandlerService(NavigationManager navigation)
+        {
+            Navigation = navigation;
+        }
+
+        /// <summary>
+        /// Zpracování přijaté výjimky
+        /// Podle typu výjimky se provede vrácení chybové hlášky a případné přesměrování uživatele
+        /// </summary>
+        /// <param name="exception">Výjimka, která má být zpracována</param>
+        /// <returns>Chybová hláška / prázdná chybová hláška + přesměrování na správnou stránku</returns>
+        public string HandleException(Exception exception)
+        {
+            if (exception is UnauthorizedAccessException)
+            {
+                Navigation.NavigateTo("/ucet/prihlaseni", true);
+            }
+            else if (exception is DbUpdateException or SqlException or TimeoutException)
+            {
+                return "Databáze není momentálně dostupná. Zkuste to prosím později. Omlouváme se za způsobené komplikace.";
+            }
+            else
+            {
+                Navigation.NavigateTo("/Error", true);
+            }
+
+            return "";
+        }
+    }
+}
