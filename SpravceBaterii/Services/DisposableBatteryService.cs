16d4694901e75bae808b9125195d03dce02e71b5
JiriKrsek <krsek.jirka@gmail.com>
2025-09-18 08:32:32 +0200

    Vytvoření služeb DisposableBatteryService a RechargeableBatteryService s CRUD operacemi

---
+++ SpravceBaterii/Services/DisposableBatteryService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpravceBaterii.Data;
+using SpravceBaterii.Data.Models;
+
+namespace SpravceBaterii.Services
+{
+    public class DisposableBatteryService
+    {
+        private readonly ApplicationDbContext ApplicationDbContext;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="applicationDbContext">ApplicationDbContext</param>
+        public DisposableBatteryService(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// Získání jednorázové baterie podle ID baterie
+        /// </summary>
+        /// <param name="batteryId">ID baterie</param>
+        /// <returns>Jednorázová baterie / null</returns>
+        public async Task<DisposableBattery?> GetDisposableBatteryById(int batteryId)
+        {
+            return await ApplicationDbContext.DisposableBatteries.AsNoTracking().FirstOrDefaultAsync(b => b.BatteryId == batteryId);
+        }
+
+        /// <summary>
+        /// Přidání jednorázové baterie do databáze
+        /// </summary>
+        /// <param name="disposableBattery">Nová jednorázová baterie</param>
+        /// <returns>Asynchronní operace</returns>
+        public async Task Add(DisposableBattery disposableBattery)
+        {
+            ApplicationDbContext.DisposableBatteries.Add(disposableBattery);
+            //Uložení
+            await ApplicationDbContext.SaveChangesAsync();
+
+            //Odpojení od slednování EF Core
+            ApplicationDbContext.Entry(disposableBattery).State = EntityState.Detached;
+        }
+
+        /// <summary>
+        /// Aktualizace jednorázové baterie
+        /// </summary>
+        /// <param name="disposableBattery">Existující jednorázová baterie</param>
+        /// <returns>Asynchronní operace</returns>
+        public async Task Update(DisposableBattery disposableBattery)
+        {
+            ApplicationDbContext.DisposableBatteries.Update(disposableBattery);
+            //Uložení
+            await ApplicationDbContext.SaveChangesAsync();
+
+            //Odpojení od slednování EF Core
+            ApplicationDbContext.Entry(disposableBattery).State = EntityState.Detached;
+        }
+
+        /// <summary>
+        /// Odstranění jednorázové baterie z databáze
+        /// </summary>
+        /// <param name="battery">Požadovaná jednorázová baterie</param>
+        /// <returns>Asynchronní operace</returns>
+        public async Task Delete(DisposableBattery battery)
+        {
+            ApplicationDbContext.DisposableBatteries.Remove(battery);
+            await ApplicationDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ SpravceBaterii/Services/RechargeableBatteryService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpravceBaterii.Data;
+using SpravceBaterii.Data.Models;
+
+namespace SpravceBaterii.Services
+{
+    public class RechargeableBatteryService
+    {
+        private readonly ApplicationDbContext ApplicationDbContext;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="applicationDbContext">ApplicationDbContext</param>
+        public RechargeableBatteryService(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        /// <summary>
+        /// Získání nabíjecí baterie podle ID baterie
+        /// </summary>
+        /// <param name="batteryId">ID baterie</param>
+        /// <returns>Nabíjecí baterie / null</returns>
+        public async Task<RechargeableBattery?> GetRechargeableBatteryById(int batteryId)
+        {
+            return await ApplicationDbContext.RechargeableBatteries.AsNoTracking().FirstOrDefaultAsync(b => b.BatteryId == batteryId);
+        }
+
+        /// <summary>
+        /// Přidání nabíjecí baterie do databáze
+        /// </summary>
+        /// <param name="rechargeableBattery">Nová nabíjecí baterie</param>
+        /// <returns>Asynchronní operace</returns>
+        public async Task Add(RechargeableBattery rechargeableBattery)
+        {
+            ApplicationDbContext.RechargeableBatteries.Add(rechargeableBattery);
+            //Uložení
+            await ApplicationDbContext.SaveChangesAsync();
+
+            //Odpojení od slednování EF Core
+            ApplicationDbContext.Entry(rechargeableBattery).State = EntityState.Detached;
+        }
+
+        /// <summary>
+        /// Aktualizace nabíjecí baterie
+        /// </summary>
+        /// <param name="rechargeableBattery">Existující nabíjecí baterie</param>
+        /// <returns>Asynchronní operace</returns>
+        public async Task Update(RechargeableBattery rechargeableBattery)
+        {
+            ApplicationDbContext.RechargeableBatteries.Update(rechargeableBattery);
+            //Uložení
+            await ApplicationDbContext.SaveChangesAsync();
+
+            //Odpojení od slednování EF Core
+            ApplicationDbContext.Entry(rechargeableBattery).State = EntityState.Detached;
+        }
+
+        /// <summary>
+        /// Odstranění nabíjecí baterie z databáze
+        /// </summary>
+        /// <param name="battery">Požadovaná nabíjecí baterie</param>
+        /// <returns>Asynchronní operace</returns>
+        public async Task Delete(RechargeableBattery battery)
+        {
+            ApplicationDbContext.RechargeableBatteries.Remove(battery);
+            await ApplicationDbContext.SaveChangesAsync();
+        }
+    }
+}
