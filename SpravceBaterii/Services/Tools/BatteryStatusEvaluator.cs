613c9ae4018e8615224501791ca64896ce3e4045
JiriKrsek <krsek.jirka@gmail.com>
2025-09-19 12:18:24 +0200

    Přesun výpočtů zbývajících dní baterie do třídy BatteryStatusEvaluator

---
+++ SpravceBaterii/Services/Tools/BatteryStatusEvaluator.cs
@@ -0,0 +1,26 @@
+﻿namespace SpravceBaterii.Services.Tools
+{
+    public class BatteryStatusEvaluator
+    {
+        /// <summary>
+        /// Výpočet zbývajících dní do konce expirace baterie
+        /// </summary>
+        /// <param name="expirationDate">Datum expirace baterie</param>
+        /// <returns>Počet dní</returns>
+        public int RemainingDaysToExpirate(DateOnly expirationDate)
+        {
+            return (expirationDate.ToDateTime(TimeOnly.MinValue) - DateTime.Today).Days;
+        }
+
+        /// <summary>
+        /// Výpočet zbývajících dní do předpokládaného data životnosti
+        /// </summary>
+        /// <param name="insertionDate">Datum vložení baterie do zařízení</param>
+        /// <param name="expectedLifespan">Předpokládaný počet dní životnosti</param>
+        /// <returns>Počet dní</returns>
+        public int RemainingDaysToExpectedLife(DateOnly insertionDate, int expectedLifespan)
+        {
+            return (insertionDate.AddDays(expectedLifespan).ToDateTime(TimeOnly.MinValue) - DateTime.Today).Days;
+        }
+    }
+}
