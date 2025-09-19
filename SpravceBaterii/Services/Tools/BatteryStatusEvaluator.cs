9eb5df2789192e77ad1f3fb8f9498fdbb7a47f4c
JiriKrsek <krsek.jirka@gmail.com>
2025-09-19 13:48:02 +0200

    Nastavení CSS třídy podle zbývajících dní baterie, obarvení

--- SpravceBaterii/Services/Tools/BatteryStatusEvaluator.cs
+++ SpravceBaterii/Services/Tools/BatteryStatusEvaluator.cs
@@ -6,10 +6,13 @@
         /// Výpočet zbývajících dní do konce expirace baterie
         /// </summary>
         /// <param name="expirationDate">Datum expirace baterie</param>
-        /// <returns>Počet dní</returns>
-        public int RemainingDaysToExpirate(DateOnly expirationDate)
+        /// <returns>Stav baterie</returns>
+        public BatteryStatus RemainingDaysToExpirate(DateOnly expirationDate)
         {
-            return (expirationDate.ToDateTime(TimeOnly.MinValue) - DateTime.Today).Days;
+            int remainingDays = (expirationDate.ToDateTime(TimeOnly.MinValue) - DateTime.Today).Days;
+            string cssClass = GetCssClass(remainingDays);
+
+            return new BatteryStatus(remainingDays, cssClass);
         }
 
         /// <summary>
@@ -17,10 +20,31 @@
         /// </summary>
         /// <param name="insertionDate">Datum vložení baterie do zařízení</param>
         /// <param name="expectedLifespan">Předpokládaný počet dní životnosti</param>
-        /// <returns>Počet dní</returns>
-        public int RemainingDaysToExpectedLife(DateOnly insertionDate, int expectedLifespan)
+        /// <returns>Stav baterie</returns>
+        public BatteryStatus RemainingDaysToExpectedLife(DateOnly insertionDate, int expectedLifespan)
         {
-            return (insertionDate.AddDays(expectedLifespan).ToDateTime(TimeOnly.MinValue) - DateTime.Today).Days;
+            int remainingDays = (insertionDate.AddDays(expectedLifespan).ToDateTime(TimeOnly.MinValue) - DateTime.Today).Days;
+            string cssClass = GetCssClass(remainingDays);
+
+            return new BatteryStatus(remainingDays, cssClass);
+        }
+
+        /// <summary>
+        /// Vyhodnocení stavu baterie podle vypočtených zbývajících dní pro stylování
+        /// </summary>
+        /// <param name="days">Počet zbývajících dní</param>
+        /// <returns>Název CSS třídy</returns>
+        private static string GetCssClass(int days)
+        {
+            if (days <= 0)
+            {
+                return "battery-expired";
+            }
+            else if (days <= 30)
+            {
+                return "battery-warning";
+            }
+            return "battery-ok";
         }
     }
 }
