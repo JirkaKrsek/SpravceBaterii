c3567fecbd5f3a427a4fcf1368ed496f9cef0a71
JiriKrsek <krsek.jirka@gmail.com>
2025-09-19 13:37:55 +0200

    Nová třída BatteryStatus pro vyhodnocený stav baterie

---
+++ SpravceBaterii/Services/Tools/BatteryStatus.cs
@@ -0,0 +1,26 @@
+﻿namespace SpravceBaterii.Services.Tools
+{
+    public class BatteryStatus
+    {
+        public int Days { get; }
+        public string CssClass { get; } = "battery-noStatus";
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public BatteryStatus()
+        {
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="days">Počet zbývajících dní</param>
+        /// <param name="cssClass">Název CSS třídy</param>
+        public BatteryStatus(int days, string cssClass)
+        {
+            Days = days;
+            CssClass = cssClass;
+        }
+    }
+}
