90dd3da2a660a823450ac35a04f564b6e92afc3f
JiriKrsek <krsek.jirka@gmail.com>
2025-10-08 23:02:36 +0200

    Vytvoření migrace - úprava tabulky Batteries

--- SpravceBaterii/Migrations/20251008205722_UpdateBatteryEntity.Designer.cs
+++ SpravceBaterii/Migrations/20251008205722_UpdateBatteryEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpravceBaterii.Data;
 
@@ -11,9 +12,11 @@ using SpravceBaterii.Data;
 namespace SpravceBaterii.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251008205722_UpdateBatteryEntity")]
+    partial class UpdateBatteryEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
