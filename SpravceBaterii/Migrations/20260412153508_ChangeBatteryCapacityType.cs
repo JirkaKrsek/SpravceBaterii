f8c6339a5d5ee7f68808b1b1ea11e8897b423ba9
JiriKrsek <krsek.jirka@gmail.com>
2026-04-12 17:40:22 +0200

    Vytvoření migrace - změna typu Capacity

---
+++ SpravceBaterii/Migrations/20260412153508_ChangeBatteryCapacityType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpravceBaterii.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeBatteryCapacityType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Capacity",
+                table: "RechargeableBatteries",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Capacity",
+                table: "RechargeableBatteries",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2,
+                oldNullable: true);
+        }
+    }
+}
